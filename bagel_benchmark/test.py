85a9a32879281be6553704eede2df7cb5de3cd62
Mandeep <mandeeprathe77@gmail.com>
2022-06-20 18:22:07 +0200

    added test

--- bagel_benchmark/test.py
+++ bagel_benchmark/test.py
@@ -1,3 +1,21 @@
 from bagel_benchmark import metrics
 from bagel_benchmark.node_classification import utils
-from bagel_benchmark import exlainers
+from bagel_benchmark.explainers.grad_based_explainers import grad_weights
+
+import torch
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+data_set="Cora"
+dataset, data, results_path = utils.load_dataset(data_set)
+data.to(device)
+
+
+model = utils.GCNNet(dataset)
+model.to(device)
+
+#### train the GNN model 
+accuracy = utils.train_model(model,data)
+print(accuracy)
+
+
+
+explanation = grad_weights(model,data)
