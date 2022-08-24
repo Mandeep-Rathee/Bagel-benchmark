3ffaf2f00deb0b28bbbf859e4f15c57cd7291b5c
Mandeep Rathee <mandeeprathee77@gmail.com>
2022-08-24 09:18:47 +0200

    Update models.py

--- bagel_benchmark/graph_classification/models.py
+++ bagel_benchmark/graph_classification/models.py
@@ -150,3 +150,25 @@ class APPNP2Net(torch.nn.Module):
         return x
 
 
+optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+criterion = torch.nn.CrossEntropyLoss()
+
+def train():
+    model.train()
+    for data in train_loader: 
+         out = model(data.x, data.edge_index, data.batch)  
+         loss = criterion(out, data.y) 
+         loss.backward()  
+         optimizer.step()  
+         optimizer.zero_grad() 
+
+def test(loader):
+     model.eval()
+     correct = 0
+     for data in loader:  
+         out = model(data.x, data.edge_index, data.batch)
+         pred = out.argmax(dim=1)  
+         correct += int((pred == data.y).sum())  
+     return correct / len(loader.dataset) 
+
+
